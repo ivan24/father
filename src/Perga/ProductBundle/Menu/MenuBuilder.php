9ec1e0b17e6244f2f2f4a5f52b3fe760195ed2b8
Ivan Oreshkov <ivan.oreshkov@itstartuplabs.com>
2013-08-31 23:23:17 +0300

    add menu

---
+++ src/Perga/ProductBundle/Menu/MenuBuilder.php
@@ -0,0 +1,30 @@
+<?php
+namespace Perga\ProductBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Главная', array('uri' => '/'));
+        $menu->addChild('Продукция', array('uri' => '/product'));
+        $menu->addChild('Контакты', array('uri' => '/contact'));
+        // ... add more children
+
+        return $menu;
+    }
+}

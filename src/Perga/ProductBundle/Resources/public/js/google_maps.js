b7e60640bac2f85f61085f5d00fb2a222f18caab
Ivan Oreshkov <ivan.oreshkov@gmail.com>
2013-09-21 11:24:29 +0300

    add google maps

---
+++ src/Perga/ProductBundle/Resources/public/js/google_maps.js
@@ -0,0 +1,46 @@
+function Maps(options) {
+    this.options = $.extend({},options,Maps.defaultOptions);
+    this.init();
+}
+Maps.prototype.init = function (options) {
+        var apiary = new google.maps.LatLng(this.options.coordinates.apiary.lat, this.options.coordinates.apiary.lon),
+            center = new google.maps.LatLng(this.options.coordinates.mapCenter.lat,this.options.coordinates.mapCenter.lon),
+            mapOptions = {
+                center: center,
+                zoom: 5,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(document.getElementById("map_canvas"),
+                mapOptions),
+            infowindow =new google.maps.InfoWindow({content: this.options.infoWindow.content}),
+            marker = new google.maps.Marker({
+                map: map,
+                draggable:true,
+                animation: google.maps.Animation.DROP,
+                position: apiary,
+                title:"Пасека Орешкова Сергея Васильевича"
+            });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+};
+
+Maps.defaultOptions = {
+    coordinates: {
+        apiary: {
+            lat: 54.375883,
+            lon: 26.382510
+        },
+        mapCenter: {
+            lat: 54.387554,
+            lon: 26.455889
+        }
+    },
+    infoWindow: {
+        content: '<div>Пасека</div>'
+    }
+};
+
+new Maps();
+
+

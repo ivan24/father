010c976a4de305b9f02b0ff7c7ce00eea9403cce
Ivan Oreshkov <ivan.oreshkov@gmail.com>
2013-10-24 23:59:32 +0300

    add tinymce editor

--- src/Perga/ProductBundle/Admin/ProductAdmin.php
+++ src/Perga/ProductBundle/Admin/ProductAdmin.php
@@ -20,8 +20,8 @@ class ProductAdmin extends Admin
             ->add('parent')
             ->add('name')
             ->add('pageTitle')
-            ->add('shortDescription')
-            ->add('description')
+            ->add('shortDescription', null, array('required' => false, 'attr' => array('class' => 'tinymce-sd')))
+            ->add('description', null, array('required' => false, 'attr' => array('class' => 'tinymce-d')))
             ->add('price')
             ->add('productOrder')
             ->add('status');
